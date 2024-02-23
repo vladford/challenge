24d07a1641e8823205eda583d5e204cc86a2da2d
Władysław Jasiński <wladyslaw.jasinski@apptsar.com>
2024-02-23 01:29:40 +0100

    Initial Commit

---
+++ ChallengeProject/ChallengeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeProjectApp.swift
+//  ChallengeProject
+//
+//  Created by Wladyslaw Jasinski on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
