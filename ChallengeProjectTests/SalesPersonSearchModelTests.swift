8a9e82869aeff329f9cf236a67c765d7aeef4b1b
Władysław Jasiński <wladyslaw.jasinski@apptsar.com>
2024-02-23 10:31:29 +0100

    Added more edge cases

--- ChallengeProjectTests/SalesPersonSearchModelTests.swift
+++ ChallengeProjectTests/SalesPersonSearchModelTests.swift
@@ -89,4 +89,70 @@ class SalesPersonSearchViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.filteredData.count, 1, "Filtered data should only contain one SalesPerson matching '99002'")
         XCTAssertEqual(viewModel.filteredData.first?.name, "Artem Titarenko", "Filtered SalesPerson should be 'Artem Titarenko'")
     }
+    
+    func testSearchFilteringWithEmptySearchText() {
+        viewModel.data = [
+            SalesPerson(name: "Artem Titarenko", areas: ["76133"]),
+            SalesPerson(name: "Bernd Schmitt", areas: ["7619*"]),
+            SalesPerson(name: "Chris Krapp", areas: ["762*"]),
+            SalesPerson(name: "Alex Uber", areas: ["86*"])
+        ]
+        
+        viewModel.debouncedSearchText = "" // Empty search text
+        
+        XCTAssertEqual(viewModel.filteredData.count, 4, "Filtered data should contain all SalesPersons when search text is empty")
+    }
+    
+    func testSearchFilteringWithNoMatch() {
+        viewModel.data = [
+            SalesPerson(name: "Artem Titarenko", areas: ["76133"]),
+            SalesPerson(name: "Bernd Schmitt", areas: ["7619*"]),
+            SalesPerson(name: "Chris Krapp", areas: ["762*"]),
+            SalesPerson(name: "Alex Uber", areas: ["86*"])
+        ]
+        
+        viewModel.debouncedSearchText = "12345" // No match
+        
+        XCTAssertEqual(viewModel.filteredData.count, 0, "Filtered data should contain no SalesPersons when search text matches no areas")
+    }
+    
+    func testSearchFilteringWithAsteriskSearchText() {
+        viewModel.data = [
+            SalesPerson(name: "Artem Titarenko", areas: ["76133"]),
+            SalesPerson(name: "Bernd Schmitt", areas: ["7619*"]),
+            SalesPerson(name: "Chris Krapp", areas: ["762*"]),
+            SalesPerson(name: "Alex Uber", areas: ["86*"])
+        ]
+        
+        viewModel.debouncedSearchText = "76*" // Matches with "76133", "7619*", and "762*"
+        
+        XCTAssertEqual(viewModel.filteredData.count, 3, "Filtered data should match areas beginning with '76'")
+    }
+    
+    func testSearchFilteringWithExactMatch() {
+        viewModel.data = [
+            SalesPerson(name: "Artem Titarenko", areas: ["76133"]),
+            SalesPerson(name: "Bernd Schmitt", areas: ["76195"]),
+            SalesPerson(name: "Chris Krapp", areas: ["76200"]),
+            SalesPerson(name: "Alex Uber", areas: ["86000"])
+        ]
+        
+        viewModel.debouncedSearchText = "76133" // Exact match
+        
+        XCTAssertEqual(viewModel.filteredData.count, 1, "Filtered data should only contain one SalesPerson matching '76133'")
+        XCTAssertEqual(viewModel.filteredData.first?.name, "Artem Titarenko", "Filtered SalesPerson should be 'Artem Titarenko'")
+    }
+    
+    func testSearchFilteringWithMultipleMatches() {
+        viewModel.data = [
+            SalesPerson(name: "Artem Titarenko", areas: ["76000", "761*"]), // Expands to 76100 to 76199
+            SalesPerson(name: "Bernd Schmitt", areas: ["76150", "762*"]), // Direct match and expands to 76200 to 76299
+            SalesPerson(name: "Chris Krapp", areas: ["76205"]),
+            SalesPerson(name: "Alex Uber", areas: ["86000"])
+        ]
+        
+        viewModel.debouncedSearchText = "761" // Matches "Artem Titarenko" and "Bernd Schmitt"
+        
+        XCTAssertEqual(viewModel.filteredData.count, 2, "Filtered data should contain two SalesPersons matching '761'")
+    }
 }
